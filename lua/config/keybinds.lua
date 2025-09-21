4ad293532db716c5a8c87997e6f631dd509ccbae
maliex4mix4 <malikopeyemi5@gmail.com>
2025-09-21 20:30:15 +0100

    keymap for integrated terminal

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>th", ":split | terminal<CR>", { desc = "Open terminal horizontally" })
+
+vim.keymap.set("n", "<leader>tv", ":vsplit | terminal<CR>", { desc = "Open terminal vertically" })
+
+vim.keymap.set("n", "<leader>tt", ":tabnew | terminal<CR>", { desc = "Open terminal in new tab" })
+
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,7 +1,7 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter", -- load when you enter insert mode
+    event = "InsertEnter",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
