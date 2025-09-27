ab6bdcdd07a06b17f0a6394f8e71e2ed1e05612f
maliex4mix4 <malikopeyemi5@gmail.com>
2025-09-27 08:34:57 +0100

    Implemented multiple color schemes

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,29 +1,32 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
     lazy = false,
     priority = 1000,
     config = function()
-      require("tokyonight").setup({
-        style = "moon", -- "storm", "night", "moon", "day"
-        transparent = false,
-        styles = {
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = {},
-          variables = {},
-        },
-        sidebars = { "qf", "help", "terminal", "packer" },
-        dim_inactive = false,
-      })
+      vim.g.moonflyTransparent = false
+      vim.g.moonflyItalics = true
+      vim.g.moonflyUnderlineMatchParen = true
+      vim.g.moonflyVirtualTextColor = true
+      vim.g.moonflyNormalFloat = true
+      vim.g.moonflyWinSeparator = 2
 
-      vim.cmd.colorscheme("tokyonight")
+      local themes = { "moonfly", "tokyonight", "catppuccin", "rose-pine" }
+      local current = 1
 
-      -- Optional highlight tweaks
-      vim.cmd('hi Directory guibg=NONE')
-      vim.cmd('hi SignColumn guibg=NONE')
+      local function cycle_theme()
+        current = (current % #themes) + 1
+        vim.cmd("colorscheme " .. themes[current])
+      end
 
+      vim.cmd("colorscheme " .. themes[current])
+      vim.api.nvim_create_user_command("ColorschemeCycle", cycle_theme, {})
+      vim.keymap.set("n", "<leader>ut", cycle_theme, { desc = "Cycle themes" })
     end,
   },
+  { "folke/tokyonight.nvim", name = "tokyonight" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "rose-pine/neovim", name = "rose-pine" },
 }
 
