64c0ee5c402fc851a3c805f493b0342d9befcf59
maliex4mix4 <malikopeyemi5@gmail.com>
2025-09-27 08:56:03 +0100

    Updated tree plugin. Also, installed NerdFont for web-devicons on iTerm

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,46 +1,54 @@
 return {
-    {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons", -- optional, for file icons
-	},
-	config = function()
-	    require("nvim-tree").setup({
-		view = {
-		    width = 30, -- sidebar width
-		    side = "left",
-		},
-		renderer = {
-		    highlight_git = true,
-		    indent_markers = { enable = true },
-		},
-		filters = {
-		    dotfiles = false, -- show hidden files if false
-		},
-	    })
-
-	    -- Keymaps
-	    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-	    -- Toggle focus between NvimTree and editor
-	    vim.keymap.set("n", "<leader>h", function()
-		if vim.bo.filetype == "NvimTree" then
-		    vim.cmd("wincmd l") -- jump right to editor
-		else
-		    vim.cmd("wincmd h") -- jump left (tree, if open)
-		end
-	    end, { noremap = true, silent = true })
-
-	    vim.keymap.set("n", "<leader>l", function()
-		if vim.bo.filetype == "NvimTree" then
-		    vim.cmd("wincmd l") -- stay consistent: right = editor
-		else
-		    vim.cmd("wincmd l") -- move right anyway
-		end
-	    end, { noremap = true, silent = true })
-
-	end,
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+      require("nvim-web-devicons").setup()
+
+      require("nvim-tree").setup({
+        view = {
+          width = 30,
+          side = "left",
+        },
+        renderer = {
+          highlight_git = true,
+          indent_markers = { enable = true },
+          icons = {
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = true,
+            },
+          },
+        },
+        filters = {
+          dotfiles = false,
+        },
+      })
+
+      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+      vim.keymap.set("n", "<leader>h", function()
+        if vim.bo.filetype == "NvimTree" then
+          vim.cmd("wincmd l")
+        else
+          vim.cmd("wincmd h")
+        end
+      end, { noremap = true, silent = true })
+
+      vim.keymap.set("n", "<leader>l", function()
+        if vim.bo.filetype == "NvimTree" then
+          vim.cmd("wincmd l")
+        else
+          vim.cmd("wincmd l")
+        end
+      end, { noremap = true, silent = true })
+    end,
+  },
 }
 
